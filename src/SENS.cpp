7eb7c7d5b84ebc817db3f59ae3a6714bcc974df5
AlexVakhnin <vakhninou@gmail.com>
2025-05-17 21:27:53 +0300

    ready for use

--- src/SENS.cpp
+++ src/SENS.cpp
@@ -5,18 +5,21 @@
 #define SENS_PERIOD 150
 
 extern double currentTemp; //температура термопары
-extern bool overShootMode;
+extern double currentTemp_b2;
+//extern bool overShootMode;
 extern unsigned long time_now;
 
 #define MAXDO   16  //MISO - data from slave  <--  (pull-up resistor 1-4.7 kom !!!)
-#define MAXCS   15  //SS - serial select to slave -->
 #define MAXCLK  14  //SC - serial clock to slave -->
+#define MAXCS   15  //SS - serial select to slave -->
+#define MAXCS_B2   15  //SS - serial select to slave -->
 
 // initialize the Thermocouple
 //Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO); //это программный SPI
 
 SPIClass myspi1(HSPI); //если требуется несколько портов SPI..
 Adafruit_MAX31855 thermocouple(MAXCS, &myspi1);
+Adafruit_MAX31855 thermocouple_b2(MAXCS_B2, &myspi1);
 
 //Adafruit_MAX31855 thermocouple(MAXCS, &SPI); //это аппаратный SPI (ссылка на обьект SPI) !!!
 
@@ -46,8 +49,14 @@ void setupSensor(){
     }
     Serial.println("DONE.");
 
-    Serial.print("Internal Temp = ");
-    Serial.println(thermocouple.readInternal());
+    if (!thermocouple_b2.begin()) {
+        Serial.println("ERROR_B2.");
+        while (1) delay(10);
+    }
+    Serial.println("DONE_B2.");
+
+    Serial.print("Internal Temp = "); Serial.println(thermocouple.readInternal());
+    Serial.print("Internal Temp B2 = "); Serial.println(thermocouple_b2.readInternal());
 
     double c = thermocouple.readCelsius();
     if (isnan(c)) {
@@ -61,6 +70,20 @@ void setupSensor(){
       Serial.print("C = ");
       Serial.println(c);
     }
+
+    c = thermocouple_b2.readCelsius();
+    if (isnan(c)) {
+      Serial.println("Thermocouple_b2 fault(s) detected!");
+      uint8_t e_b2 = thermocouple_b2.readError();
+      if (e_b2 & MAX31855_FAULT_OPEN) Serial.println("FAULT: Thermocouple_b2 is open - no connections.");
+      if (e_b2 & MAX31855_FAULT_SHORT_GND) Serial.println("FAULT: Thermocouple_b2 is short-circuited to GND.");
+      if (e_b2 & MAX31855_FAULT_SHORT_VCC) Serial.println("FAULT: Thermocouple_b2 is short-circuited to VCC.");
+
+    } else {
+      Serial.print("C_B2 = ");
+      Serial.println(c);
+    }
+
 }
 
 // получаем новые данные с термопары
@@ -87,31 +110,25 @@ void updateCurrentTemperature(){
       if (e & MAX31855_FAULT_SHORT_VCC) kind_error = -3;
       senserror++;
     }
+
+    c = thermocouple_b2.readCelsius(); //читаем термопару
+    if (!isnan(c)) { //нет ошибки
+      rawTemp_b2 = c; //запоминаем температуру без обработки для TRMPFAIL
+
+      filtTemp_b2 += (c - filtTemp_b2) * 0.2; //фильтр (IIR)
+      currentTemp_b2 = (round(filtTemp_b2*100))/100;
+      if (senserror_b2>=2) senserror_b2=1;
+      if (senserror_b2==1) {senserror_b2=0;kind_error_b2 = -4;}
+
+    } else {
+      uint8_t e_b2 = thermocouple_b2.readError();
+      if (e_b2 & MAX31855_FAULT_OPEN) kind_error_b2 = -1;
+      if (e_b2 & MAX31855_FAULT_SHORT_GND) kind_error_b2 = -2;
+      if (e_b2 & MAX31855_FAULT_SHORT_VCC) kind_error_b2 = -3;
+      senserror_b2++;
+    }
+
+
     sensLastTime = sensCurrentTime;
   }
 }
-/*
-//сдвиг элементов массива влево с добавлением нового значения
-void push_arr( double arr[], int elem, double n )
-{
-  for (int i=elem-1;i>0;i--)
-    {
-    //Serial.print(String(i)+",");
-    arr[i] = arr[i-1];
-    }
-  arr[0] = n;
-  //Serial.println(""); 
-}
-
-//сумма эдементов массива
-double sum_arr( double arr[], int elem )
-{
-  double sum = 0;
-  for (int i=0;i<elem;i++)
-    {
-    //Serial.print(String(i)+",");
-    sum=sum+arr[i];
-    }
-    return sum;
-}
-*/
