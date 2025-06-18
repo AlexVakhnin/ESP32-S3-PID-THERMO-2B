465078b38ac1b5d2480aa6180544dc0bdd9f3a5e
AlexVakhnin <vakhninou@gmail.com>
2025-06-18 09:48:31 +0300

    ready for use

--- src/ENCOD.cpp
+++ src/ENCOD.cpp
@@ -8,6 +8,8 @@
 #define DT_PIN_B2 4
 #define SW_PIN_B2 2
 
+#define STEP 5 //шаг энкодера
+
 extern volatile bool tempfail; //флаг для блокировки реле по резкому падению температуры
 extern bool overheat; //флаг для блокировки реле по перегреву
 extern volatile bool tempfail_b2;
@@ -24,13 +26,13 @@ void IRAM_ATTR rotary_encoder() {
     int currentStateCLK = digitalRead(CLK_PIN);
     if (currentStateCLK != lastStateCLK) {
         if (digitalRead(DT_PIN) != currentStateCLK) {
-            counter++;
+            counter+=STEP;//шаг вверх
             if (counter>380){counter=380;} //ограничение вверх
-            else if (counter==1){counter=100;} //пропуск (1-149) при увеличении
+            else if (counter==STEP){counter=100;} //пропуск (1-99) при увеличении
         } else {
-            counter--;
+            counter-=STEP;//шаг вниз
             if (counter<0){counter=0;} //ограничение вниз
-            else if (counter==99){counter=0;} //пропуск (1-149) при уменьшении
+            else if (counter==100-STEP){counter=0;} //пропуск (1-99) при уменьшении
         }
     }
     lastStateCLK = currentStateCLK;
@@ -40,13 +42,13 @@ void IRAM_ATTR rotary_encoder_b2() {
     int currentStateCLK = digitalRead(CLK_PIN_B2);
     if (currentStateCLK != lastStateCLK_b2) {
         if (digitalRead(DT_PIN_B2) != currentStateCLK) {
-            counter_b2++;
+            counter_b2+=STEP;//шаг вверх
             if (counter_b2>360){counter_b2=360;} //ограничение вверх
-            else if (counter_b2==1){counter_b2=100;} //пропуск (1-99) при увеличении
+            else if (counter_b2==STEP){counter_b2=100;} //пропуск (1-99) при увеличении
         } else {
-            counter_b2--;
+            counter_b2-=STEP;//шаг вниз
             if (counter_b2<0){counter_b2=0;} //ограничение вниз
-            else if (counter_b2==99){counter_b2=0;} //пропуск (1-99) при уменьшении
+            else if (counter_b2==100-STEP){counter_b2=0;} //пропуск (1-99) при уменьшении
         }
     }
     lastStateCLK_b2 = currentStateCLK;
